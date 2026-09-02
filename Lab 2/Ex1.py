99c5c4ef2d955609e1257590b337380c6f9475e9
Mahea <aylaalam@hawaii.edu>
2026-09-02 12:57:53 -1000

    Exersize 2
    Its finally working!! 😭

--- Lab 2/Ex1.py
+++ Lab 2/Ex1.py
@@ -1,4 +1,8 @@
-value_entered = input("Enter a number between 1 and 100: ")
+# This program prompts the user to enter a number between 1 and 100, converts the input to an integer, calculates the square of that number, and then prints both the original number and its square.
+# number, and then print both the entered number and its square.
+#Name: Mahealani Alameida
+#Date: 9/2/2024
+# value_entered = input("Enter a number between 1 and 100: ")
 value_as_integer = int(value_entered)
 
 valueSquared = value_as_integer ** 2
---
+++ Lab 2/Ex2.py
@@ -0,0 +1,10 @@
+#ask the user to enter thier birth year. calculate the age based ont he current year and print it out
+#Name: Mahealani Alameida
+#Date: 9/2/2024
+
+birth_year = input("Enter your birth year: ")
+current_year = 2026
+age = current_year - int(birth_year)
+
+print("you entered:", birth_year)
+print("your age is:", age)
