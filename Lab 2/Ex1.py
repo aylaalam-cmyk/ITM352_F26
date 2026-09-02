d3375e073b49e9e86e7ca509813094ad6233d664
Mahea <aylaalam@hawaii.edu>
2026-09-02 12:44:07 -1000

    I cant work the terminal 😭

--- Lab 2/Ex1.py
+++ Lab 2/Ex1.py
@@ -1,2 +1,9 @@
 value_entered = input("Enter a number between 1 and 100: ")
 value_as_integer = int(value_entered)
+
+valueSquared = value_as_integer ** 2
+
+print("you entered:", value_as_integer)
+print("the square of your number is:", valueSquared)
+
+print(f"you entered: {value_as_integer}, and the square of your number is: {valueSquared}")
