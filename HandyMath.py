856d7c291f4a8887d9825f23702af138d8033e40
Mahea <aylaalam@hawaii.edu>
2026-09-11 13:17:27 -1000

    newww

---
+++ HandyMath.py
@@ -0,0 +1,24 @@
+def midpoint(number1, number2):
+    return (number1 + number2) / 2
+
+
+def squareroot(number):
+    return number ** 0.5
+
+
+def max(num1, num2):
+    return num1 if num1 > num2 else num2
+
+
+def min(num1, num2):
+    return num1 if num1 < num2 else num2
+
+
+'''
+exponent, which takes two numbers as input and returns the first number raised to the power of the second number.
+'''
+
+
+
+def exponent(base, exponent):
+    return base ** exponent
