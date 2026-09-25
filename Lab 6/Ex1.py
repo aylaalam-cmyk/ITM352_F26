bab0d8ea158424fff42006b94feeeee6ea20134c
Mahea <aylaalam@hawaii.edu>
2026-09-25 12:43:36 -1000

    update

--- Lab 6/Ex1.py
+++ Lab 6/Ex1.py
@@ -2,4 +2,10 @@
 #Name: Mahealani Alameida
 #Date: 9/25/2026
 
-emotions = ("happy", "sad", "fear", "suprise")
+emotions = ("suprise", "sad", "fear", "happy")
+is_it_true = emotions[len(emotions)-1] == "happy"and len(emotions) > 3
+print(is_it_true)
+#print(emotiSons[3] ++ "happy" and len(emotions) > 3)
+
+
+
